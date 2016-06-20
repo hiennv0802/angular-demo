9b26f07d95f9821552cf49a1ca9d5f25986f1935
Tuan Nguyen <tuanna2704@gmail.com>
2016-06-20 16:06:35 +0700

    ui-router

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,6 @@
 //= require turbolinks
 //= require lib/angular.min.js
 //= require lib/angular-route.min.js
+//= require lib/angular-ui-router.min.js
 //= require angular/app.js
+//= require angular/controllers/searchController.js
